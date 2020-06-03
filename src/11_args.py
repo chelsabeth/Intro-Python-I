b32802f9618e8f18bd97b26196847c8aa2a30d51
Chelsea Wetzel <chelswetz@gmail.com>
2020-06-03 12:23:59 -0400

    args.py

--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,9 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x, y):
+    # without return funtion will return None
+    return x + y 
 
 print(f1(1, 2))
 
@@ -13,6 +16,11 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args): # *args will represent an arbitrary amount of args
+    sum = 0
+    for x in args: 
+        sum += x
+    return sum 
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +30,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +38,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y=1): # if we pass in single arg it will be x, if second arg it will be b
+    return x + y
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +54,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs): # kwargs = key word args - come through as dict
+    for key, val in kwargs.items(): # get each key value pair
+        print(f'key: {key}, value: {val}')
 
 # Should print
 # key: a, value: 12
@@ -62,4 +75,4 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+f4(**d) # specify that its coming through as a keyword dict list
